07856976757277245cc409c4f5153a78d9d44370
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-08-28 12:37:08 +0200

    add 28 August work

--- js/main.js
+++ js/main.js
@@ -1,34 +1,119 @@
 
-function verification(ajout) {
+// function verification(ajout) {
+//
+//   var all = "1234567890+-.*/";
+//
+//   for (var i = 0; i < ajout.length; i++){
+//
+//   if (all.indexOf(ajout.charAt(i)) < 0 )
+//      return false;
+//      return true;
+//    }
+//    /*
+//    Indexof renvoit à la position du caractère de la chaine.
+//    charAt au choix du caractère.
+//    */
+//  }
+//
+//  function calcul() {
+//
+//    var final = 0;
+//      if (verification(document.calculatrice.result.value)){
+//        final = eval(document.calculatrice.result.value);
+//        /*Eval execute la fonction de ce qui est contenu dans la chaine*/
+//        document.calculatrice.result.value = final;
+//      }
+//  }
+//
+//  function ajouter(caracteres) {
+//    /*permet la concaténation des caractères*/
+//    document.calculatrice.result.value =
+//    document.calculatrice.result.value + caracteres;
+//  }
 
-  var all ="1234567890[]()+-.*,/";
+var num1 ="";
+var num2="";
 
-  for (var i = 0; i < ajout.length; i++){
+var result="";
 
-  if (all.indexOf(ajout.charAt(i)) < 0 )
-     return false;
-     return true;
-   }
-   /*
-   Indexof renvoit à la position du caractère de la chaine.
-     charAt renvoit à l'index choisit.
-   */
- }
+var dot=0;
 
- function calcul() {
+var display = document.getElementById("display");
 
-   var a = 0;
-   if (verification(document.calculatrice.result.value)){
-     a = eval(document.calculatrice.result.value);
-     /*Eval prend en compte la chaine de caractère*/
-     document.calculatrice.result.value = a;
-   }
+//reinitialization
+function restart(){
+  num1 = display.innerHTML = "" ;
+  num2="";
+  result="";
+}
 
- }
+//function +/-
+function inversion(){
+  //need to be a number to work with -1 multiplication
+  num1=Number(num1);
+  num1 = display.innerHTML = num1*(-1);
+  num1=num1.toString();
+}
 
- function ajouter(caracteres) {
+//display number
+function add(n){
+  num1 = display.innerHTML = num1 + n ;
+}
 
-   /*permet la concaténation*/
-   document.calculatrice.result.value =
-   document.calculatrice.result.value + caracteres;
- }
+//display dot
+function dot(n){
+  if (dot===0){
+    num1 = display.innerHTML = num1 + n ;
+    dot++;
+  }
+}
+
+//main operator system
+function ope(n){
+
+  //saving first value
+  if (num2===""){
+    num2 = Number(num1);
+    //reinitializing dot for each number
+    dot=0;
+  }
+
+  //comparing previous operator to make the operation correctly
+  else {
+    calcul();
+    //saving new value
+    num2=num1;
+    dot=0;
+  }
+
+  //reinitializing input value
+  num1="";
+
+  //saving operator for the next operation
+  result= n ;
+  return result;
+}
+
+//only "=" button
+function result(){
+  calcul();
+  dot=0;
+  num2="";
+  result="";
+}
+
+function calcul(){
+  num1=Number(num1);
+  if (result==="+"){
+    num1=display.innerHTML = num2 + num1;
+  }
+  else if (result==="-"){
+    num1=display.innerHTML = num2 - num1 ;
+  }
+  else if (result==="x"){
+    num1=display.innerHTML = num2 * num1 ;
+  }
+  else if (result==="/"){
+    num1=display.innerHTML = num2/ num1 ;
+  }
+}
